b87e518d29d0325bd31e819f8cf1c1d8904e14a0
Rafael Alexander Muijsert <rafael@muijsert.org>
2025-01-15 12:13:21 +0100

    Add optional timeout parameter

--- gitscanner/gitscanner.py
+++ gitscanner/gitscanner.py
@@ -12,22 +12,24 @@ from pathlib import Path
 
 import requests
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+DEFAULT_TIMEOUT = 5
 
-def git_exposed(url: str) -> bool:
+
+def git_exposed(url: str, timeout: float = DEFAULT_TIMEOUT) -> bool:
     """Check whether the site located at URL contains an exposed Git repository.
 
     Arguments:
         url (str): The URL which should be checked. Must end with a /.
+        timeout (float): Timeout when checking URLs.
 
     Returns:
         bool: True if the URL contains an exposed repository, otherwise False.
 
     """
     try:
-        response = requests.get(url + ".git", timeout=1)
+        response = requests.get(url + ".git", timeout=timeout)
     except requests.RequestException as err:
         logger.debug(err)
         return False
@@ -51,29 +53,31 @@ def load_urls(file_path: str) -> list[str]:
         return contents
 
 
-def get_exposed_repositories(urls: list[str]) -> list[str]:
-    """Get each exposed repository from a dictionary of groups.
-
-    Arguments:
-        urls (dict): A dictionary in the form group->members
-
-    Returns:
-        list: A list containing the URLs of exposed repositories from groups.
-
-    """
-    exposed: list[str] = [url for url in urls if git_exposed(url)]
-    return exposed
-
-
 def main() -> None:
     """Evaluate each URL combination."""
     parser = argparse.ArgumentParser(description="Scan for exposed Git repos")
     _ = parser.add_argument("filename")
+    _ = parser.add_argument("-v", "--verbose", action="store_true")
+    _ = parser.add_argument(
+        "-t",
+        "--timeout",
+        nargs="?",
+        const=DEFAULT_TIMEOUT,
+        type=float,
+    )
     args = parser.parse_args()
+
+    verbose: bool = args.verbose  # pyright: ignore[reportAny]
+    logging.basicConfig(level=logging.INFO if verbose else logging.WARNING)
 
     file_name: str = args.filename  # pyright: ignore[reportAny]
     urls = load_urls(file_name)
-    exposed_repositories = get_exposed_repositories(urls)
+
+    timeout: float = args.timeout  # pyright: ignore[reportAny]
+
+    exposed_repositories: list[str] = [
+        url for url in urls if git_exposed(url, timeout=timeout)
+    ]
     for url in exposed_repositories:
         _ = sys.stdout.write(url + "\n")
 
